43ee1376efc856404e5a04a40425cdae3bffa16b
saikumar7 <kumar.vits15@gmail.com>
2020-02-25 19:21:56 -0500

    update

---
+++ src/app/core/token-injector.ts
@@ -0,0 +1,12 @@
+import {InjectionToken} from "@angular/core";
+import * as Highcharts from 'highcharts';
+require('highcharts/modules/sankey')(Highcharts);
+
+export function highchartFactory(){
+  return Highcharts;
+}
+
+const HIGHCHART_TOKENS: InjectionToken<any> = new InjectionToken<any>('highchart');
+const HIGHCHART_PROVIDER = { provide: HIGHCHART_TOKENS, useFactory: highchartFactory };
+
+export { HIGHCHART_TOKENS, HIGHCHART_PROVIDER };
